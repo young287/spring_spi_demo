1479ffb467335087970f89540cae606d3b4e17c2
young287 <601902897@qq.com>
2022-03-18 16:05:25 +0800

    测试

--- src/main/java/spring/Demo.java
+++ src/main/java/spring/Demo.java
@@ -1,10 +1,12 @@
 package spring;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import spring.annotation.SpiAnnotation;
 
 @Component
 @SpiAnnotation(value = "Spi_Importselect")
+
 public class Demo {
 
 }
